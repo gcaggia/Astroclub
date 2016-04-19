785fd801bb77055dd4f5e20861a98d294a50f0cc
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-19 15:07:14 +0000

    use of ob_start to generate part of the view

--- template.php
+++ template.php
@@ -4,24 +4,24 @@
 <head>
     <meta charset="UTF-8" />
     <link rel="stylesheet" href="style.css" />
-    <title>William Shakespeare Poems</title>
+    <title><?php echo $title; ?></title>
 </head>
 
 <body>
-<div id="global">
-    <header>
-        <a href="index.php"><h1 id="titlePage">William Shakespeare Poems</h1></a>
-        <p>This is a select list of the best famous William Shakespeare poetry</p>
-    </header>
-    <div id="content">
-        <?php echo $content; ?> <!-- Content of the webpage -->
+    <div id="global">
+        <header>
+            <a href="index.php"><h1 id="titlePage">William Shakespeare Poems</h1></a>
+            <p>This is a select list of the best famous William Shakespeare poetry</p>
+        </header>
+        <div id="content">
+            <?php echo $content; ?> <!-- Content of the webpage -->
+        </div>
+        <!-- #content -->
+        <footer id="footerPage">
+            Website performed with PHP, HTML5 and CSS.
+        </footer>
     </div>
-    <!-- #content -->
-    <footer id="footerPage">
-        Website performed with PHP, HTML5 and CSS.
-    </footer>
-</div>
-<!-- #global -->
+    <!-- #global -->
 </body>
 
 </html>
--- viewIndex.php
+++ viewIndex.php
@@ -1,35 +1,19 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="style.css" />
-    <title>William Shakespeare Poems</title>
-</head>
 
-<body>
-<div id="global">
-    <header>
-        <a href="index.php"><h1 id="titlePage">William Shakespeare Poems</h1></a>
-        <p>This is a select list of the best famous William Shakespeare poetry</p>
-    </header>
-    <div id="content">
-        <?php foreach ($poems as $poem): ?>
-            <article>
-                <header>
-                    <h1 class="titlePoem"><?= $poem['title'] ?></h1>
-                </header>
-                <p> <?php echo $poem['content'] ?> </p>
-            </article>
-            <hr />
-        <?php endforeach; ?>
-    </div>
-    <!-- #content -->
-    <footer id="footerPage">
-        Website performed with PHP, HTML5 and CSS.
-    </footer>
-</div>
-<!-- #global -->
-</body>
+<?php $title = "William Shakespeare Poems"; ?>
 
-</html>
+<?php ob_start(); ?>
+
+<?php foreach ($poems as $poem): ?>
+    <article>
+        <header>
+            <h1 class="titlePoem"><?= $poem['title'] ?></h1>
+        </header>
+        <p> <?php echo $poem['content'] ?> </p>
+    </article>
+    <hr />
+<?php endforeach; ?>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'template.php'; ?>
