134038d9a94a3065d1fd30d19d05ebae0ad65e60
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-19 20:44:54 +0000

    update of model.class.php with specific function for inheritance

--- Model/Model.class.php
+++ Model/Model.class.php
@@ -1,61 +1,32 @@
 <?php
 
-class Model {
+abstract class Model {
     
-    // return information of a specific poem
-    public function getaPoem($idPoem) {
-        $db = getDb();
-                  
-        $poem = $db->prepare('SELECT POEM_ID      as id,     ' . 
-                             '       POEM_TITLE   as title,  ' . 
-                             '       POEM_CONTENT as content ' . 
-                             'FROM T_POEM                    ' . 
-                             'WHERE POEM_ID = ? ');
-        
-        $poem->execute(array($idPoem));
-        
-        if($poem->rowCount() == 1) {
-            return $poem->fetch();  // return the first result 
+    // PDO object to access to the database
+    private $db;
+    
+    // Execute an sql query with potential parameters
+    protected function executeQuery($sql, $params = null) {
+        if ($params == null) {
+            $result = $this->getDb()->query($sql);     // direct execution
         } else {
-            throw new Exception("No poem correspond to the id '$idPoem'");
+            $result = $this->getDb()->prepare($sql);   // prepare query
+            $result->execute($params);
         }
+        
+        return $result;
     }
     
-    // return the list of comments from a specific poem
-    public function getComments($idPoem) {
-        $db = getDb();
-        
-        $comments = $db->prepare('SELECT COM_ID      as id,     ' . 
-                                 '       COM_DATE    as date,   ' . 
-                                 '       COM_AUTHOR  as author, ' . 
-                                 '       COM_CONTENT as content ' . 
-                                 'FROM T_COMMENT                ' . 
-                                 'WHERE COM_ID = ? ');
-        
-        $comments->execute(array($idPoem));
-        
-        return $comments;
-    }
     
-    // return the list of poems, sorted by id desc
-    public function getPoems() {
-        $db = getDb();
-                  
-        $poems = $db->query('SELECT POEM_ID      as id,     ' . 
-                            '       POEM_TITLE   as title,  ' . 
-                            '       POEM_CONTENT as content ' . 
-                            'FROM T_POEM                    ' . 
-                            'ORDER BY POEM_ID desc');
-        
-        return $poems;
-    }
-    
-    // connect to the database and return PDO object
+    // connect to the database and return PDO object, initialize when needed
     private function getDb() {
-        $db = new PDO('mysql:host=localhost;dbname=shakespeare;charset=utf8', 
-                  'gcaggia', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         
-        return $db;
+        if($this->db == null) {
+            $this->db = new PDO('mysql:host=localhost;dbname=shakespeare;charset=utf8', 
+                  'gcaggia', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+
+        return $this->db;
     }
     
 }
