0d429900774dcc197caa2a3cd6ce7abc598d4a40
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-19 20:34:44 +0000

    creation of class model comment

--- Model/Comment.class.php
+++ Model/Comment.class.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once "Model/Model.class.php";
+
+class Comment extends Model {
+    
+    // return the list of comments from a specific poem
+    public function getComments($idPoem) {
+        
+        $sql = 'SELECT COM_ID      as id,     ' . 
+               '       COM_DATE    as date,   ' . 
+               '       COM_AUTHOR  as author, ' . 
+               '       COM_CONTENT as content ' . 
+               'FROM T_COMMENT                ' . 
+               'WHERE COM_ID = ? ';
+        
+        $comments = $this->executeQuery($sql, array($idPoem));
+        
+        return $comments;
+    }
+    
+}
