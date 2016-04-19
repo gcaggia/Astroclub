a0f32343bf66d14e88f0330522a4ca1d0ef0f28a
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-19 14:56:48 +0000

    creation of a template for the view

---
+++ template.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>William Shakespeare Poems</title>
+</head>
+
+<body>
+<div id="global">
+    <header>
+        <a href="index.php"><h1 id="titlePage">William Shakespeare Poems</h1></a>
+        <p>This is a select list of the best famous William Shakespeare poetry</p>
+    </header>
+    <div id="content">
+        <?php echo $content; ?> <!-- Content of the webpage -->
+    </div>
+    <!-- #content -->
+    <footer id="footerPage">
+        Website performed with PHP, HTML5 and CSS.
+    </footer>
+</div>
+<!-- #global -->
+</body>
+
+</html>
