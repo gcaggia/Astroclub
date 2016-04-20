6629d926918f7c041afedcb683dcdf7c0102119a
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-20 15:27:36 +0000

    modification of index.php : instanciation of router

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php 
 
-require "Controller/controller.php";
+require "Controller/Router.class.php";
 
-try {
-    
-    if(!isset($_GET['action'])) {
-       index();
-       return;
-    }    
-        
-    if ($_GET['action'] !== 'poem') {
-      throw new Exception("Invalid action.");
-    }
-    
-    if (!isset($_GET['id'])) {
-      throw new Exception("No Poem ID.");
-    }
-    
-    $idPoem = intval($_GET['id']);
-    
-    if ($idPoem === 0) {
-        throw new Exception("Poem ID not correct...");
-    }
-    
-    // We call the controller
-    poem($idPoem);
- 
-    
-} catch (Exception $e) {
-    error($e->getMessage());
-}
+$router = new Router();
+$router->queryRouter();
