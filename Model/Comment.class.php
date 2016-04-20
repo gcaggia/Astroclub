3c284d64a59bd9a9e86671d66c88c0ccc0b370dd
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-20 17:00:21 +0000

    add addComment function to Comment model

--- Model/Comment.class.php
+++ Model/Comment.class.php
@@ -4,7 +4,7 @@ require_once "Model/Model.class.php";
 
 class Comment extends Model {
     
-    // return the list of comments from a specific poem
+    // Return the list of comments from a specific poem
     public function getComments($idPoem) {
         
         $sql = 'SELECT COM_ID      as id,     ' . 
@@ -17,6 +17,20 @@ class Comment extends Model {
         $comments = $this->executeQuery($sql, array($idPoem));
         
         return $comments;
+    }
+    
+    // Add a comment to the database
+    public function addComment($author, $content, $idPoem) {
+        
+        $sql = 'INSERT INTO T_COMMENT( COM_DATE, COM_AUTHOR,  ' .
+               '                       COM_CONTENT, POEM_ID ) ' .
+               'VALUES (?, ?, ?, ?) ';
+        
+        $dt = date(DATE_W3C); // Get current date
+        
+        $this->executeQuery($sql, array($dt, $author, 
+                                        $content, $idPoem));
+        
     }
     
 }
