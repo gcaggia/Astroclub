76fda4b0fcc556cc69f0d8373c81ab6920089c12
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-22 09:29:12 +0000

    Creation of config class

---
+++ Configuration/Configuration.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class Configuration {
+    
+    private static $parameters;
+
+    // Return value of a param confing
+    public static function get($name, $defaultValue = null) {
+        
+        if(isset(self::getParam()[$name])) {
+            $value = self::getParam()[$name];
+        } else {
+            $value = $defaultValue;
+        }
+        
+        return $value;
+        
+    }
+    
+    // Return Array of parameters and load it when needed
+    private static function getParam() {
+        
+        if (self::$parameters == null) {
+            
+            $pathFile = "Configuration/prod.ini";
+            
+            if(!file_exists($pathFile)) {
+                $pathFile = "Configuration/dev.ini";
+            }
+            
+            if(!file_exists($pathFile)) {
+                throw new Exception "Config file not found";
+            } else {
+                self::$parameters = parse_ini_file($pathFile);
+            }
+            
+        } 
+        
+        return self::$parameters;
+        
+    }
+    
+    
+}
