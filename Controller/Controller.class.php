1b8bbc04a113ec0f19aafd68f4607ce25bba2cc5
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-23 10:12:08 +0000

    Creation of Controller class

---
+++ Controller/Controller.class.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once "Controller/Request.class.php";
+require_once "View/View.class.php";
+
+abstract class Controller {
+    
+    // Action to do
+    private $action;
+    
+    //Incoming Request
+    protected $request;
+    
+    // Define incoming request
+    public function setRequest(Request $request) {
+        $this->request = $request;
+    }
+    
+    // Execute action to do
+    public function executeAction($action) {
+        
+        if(method_exists($this, $action)) {
+            $this->action = $action;
+            $this->{$this->action}();
+        } else {
+            $controllerClass = get_class($this);
+            throw new Exception("Action '$action' undefined 
+                inside $controllerClass class");
+        }
+    }
+    
+    // Abstract method corresponding to default action
+    // Force sub class to implemente it
+    public abstract function index();
+    
+    //Generate View associate to current Controller
+    protected function generateView($viewData = array()) {
+        
+        // Determine name of the view file from name of current controller
+        $controllerClass = get_class($this);
+        $controllerName  = str_replace("Controller", "", $controllerClass);
+        
+        // Instanciation and generation of the view
+        $view = new View($this->action, $controller);
+        $view->generate($viewData);
+        
+    }
+    
+}
